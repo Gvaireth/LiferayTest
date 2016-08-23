d34b25eabe4792b516b3b8a6f663d8cc644ec4a8
gvaireth <pzajaczkowski@gmail.com>
2016-08-23 13:06:48 +0200

    spring boot draft

---
+++ src/main/java/org/gvaireth/liferaytest/LiferaytestApplication.java
@@ -0,0 +1,13 @@
+package org.gvaireth.liferaytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiferaytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiferaytestApplication.class, args);
+		System.out.println("app running!");
+	}
+}
