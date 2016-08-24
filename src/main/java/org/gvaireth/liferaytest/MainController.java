e55edbead092445026d75a35a80ff9a085a0a203
gvaireth <pzajaczkowski@gmail.com>
2016-08-24 08:50:18 +0200

    spring mvc up and running

---
+++ src/main/java/org/gvaireth/liferaytest/MainController.java
@@ -0,0 +1,16 @@
+package org.gvaireth.liferaytest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/")
+	public String helloWorld(Model model) {
+		System.out.println("front controller up");
+		return "/start.jsp";
+	}
+
+}
