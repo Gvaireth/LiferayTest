ee2e4f9b5a832450ca7794f5dc4a94338bff5a94
gvaireth <pzajaczkowski@gmail.com>
2016-08-25 11:13:48 +0200

    main controler cleanup, liferay and portlet dependencies added

--- src/main/java/org/gvaireth/liferaytest/MainController.java
+++ src/main/java/org/gvaireth/liferaytest/MainController.java
@@ -7,13 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
-	@RequestMapping("/")
-	public String helloWorld(Model model) {
-		System.out.println("front controller up");
-		return "/start.jsp";
-	}
-
-	@RequestMapping("/foo")
+	@RequestMapping("/start")
 	public String helloWorld2(Model model) {
 		System.out.println("front controller up");
 		return "/start.jsp";
